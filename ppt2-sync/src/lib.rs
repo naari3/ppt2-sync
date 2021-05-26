6e6aca5671c371271016f46b2357829bca9ad608
naari3 <naari.named@gmail.com>
2021-05-27 03:16:33 +0900

    Simple dll for inject

--- ppt2-sync/src/lib.rs
+++ ppt2-sync/src/lib.rs
@@ -1,7 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[warn(non_snake_case)]
+use winapi::shared::minwindef::*;
+
+use winapi::um::winnt::DLL_PROCESS_ATTACH;
+use winapi::um::winuser::{MessageBoxW, MB_ICONINFORMATION, MB_OK};
+
+#[no_mangle]
+pub unsafe extern "system" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> BOOL {
+    match reason {
+        DLL_PROCESS_ATTACH => {
+            let s = "DLL_PROCESS_ATTACH\0".to_string();
+            msg(&s);
+        }
+        _ => {
+            // let s = "DEFAULT\0".to_string();
+            // msg(&s);
+        }
+    }
+    TRUE
+}
+
+fn msg(caption: &str) {
+    let lp_text: Vec<u16> = "Hello World! \u{1F60E}\0".encode_utf16().collect();
+    let lp_caption: Vec<u16> = caption.encode_utf16().collect();
+
+    unsafe {
+        MessageBoxW(
+            std::ptr::null_mut(),
+            lp_text.as_ptr(),
+            lp_caption.as_ptr(),
+            MB_OK | MB_ICONINFORMATION,
+        );
     }
 }
